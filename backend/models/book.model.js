92edad557f65f41293f3f202370d3fbd35dad468
Juan Vidal <juanmvg2003@gmail.com>
2023-10-21 15:42:46 -0500

    It was created model and schema of book

---
+++ backend/models/book.model.js
@@ -0,0 +1,26 @@
+// import mopngoose
+import mongoose from "mongoose";
+// book shema
+const bookModel = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  author:{
+    type: String,
+    trim: true,
+    required: true
+  },
+  publishYear:{
+    type: Number,
+    required: true,
+    trim: true
+  }
+},{
+  timestamps: true
+})
+
+
+// book model
+export const Book = mongoose.model('Book', bookModel);
