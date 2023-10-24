db3d2a4cd2db87972031a39d3566c360f1fcd17d
Juan Vidal <juanmvg2003@gmail.com>
2023-10-23 19:40:26 -0500

    It was created react project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+// import react router DOM
+import { Routes, Route } from "react-router-dom"
+// import page components
+import Home from "./pages/Home"
+import CreateBook from "./pages/CreateBook"
+import DeleteBook from "./pages/DeleteBook"
+import ShowBooks from "./pages/ShowBooks"
+import UpdateBook from "./pages/UpdateBook"
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/books/create" element={<CreateBook />}/>
+      <Route path="/books/details/:id" element={<ShowBooks />}/>
+      <Route path="/books/edit/:id" element={<UpdateBook />}/>
+      <Route path="/books/delete/:id" element={<DeleteBook />}/>
+    </Routes>
+  )
+}
+
+export default App
